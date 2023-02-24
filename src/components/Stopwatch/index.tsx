cfa9b7b6f50e01696be01afe8822d0e235135843
oricardos <ricardo.rs95@gmail.com>
2023-02-23 23:04:37 -0300

    feat(stopwatch): convertendo tempo para segundos

--- src/components/Stopwatch/index.tsx
+++ src/components/Stopwatch/index.tsx
@@ -1,8 +1,10 @@
+import { timeToSeconds } from "../../common/utils/date"
 import Button from "../Button"
 import { Clock } from "./Clock"
 import style from './Stopwatch.module.scss'
 
 export const Stopwatch = () => {
+    console.log('conversão: ', timeToSeconds('01:01:01'))
     return (
         <div className={style.stopwatch}>
         <p className={style.title}>Escolha um card e inicie o cronômetro</p>
