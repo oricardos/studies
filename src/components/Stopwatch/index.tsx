66fd3868aa941e82d8fd942b7f3e3644957a1aff
oricardos <ricardo.rs95@gmail.com>
2023-02-24 16:27:55 -0300

    feat(stopwatch): iniciando cronometro

--- src/components/Stopwatch/index.tsx
+++ src/components/Stopwatch/index.tsx
@@ -1,13 +1,24 @@
-import { timeToSeconds } from "../../common/utils/date"
+import { useState } from "react"
+import { timeToSeconds } from "../../common/utils/time"
+import { ITask } from "../../types/tasks"
 import Button from "../Button"
 import { Clock } from "./Clock"
 import style from './Stopwatch.module.scss'
 
-export const Stopwatch = () => {
-    console.log('conversão: ', timeToSeconds('01:01:01'))
+interface Props {
+    selected: ITask | undefined
+}
+
+export const Stopwatch = ({selected}: Props) => {
+    const [time, setTime] = useState<number>()
+
+    if(selected?.time){
+        setTime(timeToSeconds(selected?.time))
+    }
     return (
         <div className={style.stopwatch}>
         <p className={style.title}>Escolha um card e inicie o cronômetro</p>
+        Tempo: {time}
 
         <div className={style.clockWrapper}>
             <Clock />
